6b2db15ff1db6b4b843b7fa3d411aad9bdef7b51
Seth Samuel <sethfsamuel@gmail.com>
2015-08-28 19:15:02 -0400

    Initial Commit

---
+++ SimpleMessaging/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleMessaging
+//
+//  Created by Seth on 8/28/15.
+//  Copyright (c) 2015 Seth Samuel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
